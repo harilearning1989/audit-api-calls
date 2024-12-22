256b9215a9fed43d6d4cc43d6cf53c7219247973
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2024-12-22 16:52:35 +0530

    modified

--- common-utils/src/main/java/com/web/common/repos/ClientAuditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.common.repos;
-
-import com.web.common.entities.ClientAudit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientAuditRepository extends JpaRepository<ClientAudit, String> {
-}
--- common-utils/src/main/java/com/web/common/repos/ServiceAuditRepository.java
+++
@@ -1,8 +0,0 @@
-package com.web.common.repos;
-
-import com.web.common.entities.ServiceAudit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceAuditRepository extends JpaRepository<ServiceAudit, String> {}
